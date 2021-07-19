2205df58dd6f1772ba20a396666b5ca564041f8e
authdwp1 <87630242+authdwp1@users.noreply.github.com>
2021-07-18 23:07:17 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,9 +1,9 @@
-firebase.initializeApp({
- apiKey: "AIzaSyAwNTgziEN6Hq1hgDIpTx31t1V4pKi6N48",
+ var firebaseConfig = {
+    apiKey: "AIzaSyAwNTgziEN6Hq1hgDIpTx31t1V4pKi6N48",
     authDomain: "authdwp-1.firebaseapp.com",
     projectId: "authdwp-1",
     storageBucket: "authdwp-1.appspot.com",
     messagingSenderId: "909409644571",
     appId: "1:909409644571:web:1279d5a8818bb1ef0548be",
     measurementId: "G-WC0SNSY7LJ"
-});
+  };
